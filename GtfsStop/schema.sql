f21d2fc89932b656a7dea17bd0031c92eb2948a1
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:42:17 +0100

    test of SQL export

---
+++ GtfsStop/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model GtfsStop of the subject dataModel.UrbanMobility for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE GtfsStop_type AS ENUM ('GtfsStop');CREATE TYPE wheelChairAccessible_type AS ENUM (0, 1, 2);
+CREATE TABLE GtfsStop (address json, alternateName text, areaServed text, code text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasParentStation text, hasService text, hasStop json, hasTrip text, id text, location json, name text, operatedBy json, owner json, page json, seeAlso json, source text, stop_desc text, type GtfsStop_type, wheelChairAccessible wheelChairAccessible_type, zoneCode text);
