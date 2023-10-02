1ba2174485d5792f7a9391395964952b2062b996
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:26:48 +0200

    test of SQL export

--- GtfsStop/schema.sql
+++ GtfsStop/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model GtfsStop of the subject dataModel.UrbanMobility for a postgreSQL database. Pending translation of enumerations and multityped attributes */
-CREATE TYPE GtfsStop_type AS ENUM ('GtfsStop');CREATE TYPE wheelChairAccessible_type AS ENUM (0, 1, 2);
-CREATE TABLE GtfsStop (address json, alternateName text, areaServed text, code text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, hasParentStation text, hasService text, hasStop json, hasTrip text, id text, location json, name text, operatedBy json, owner json, page text, seeAlso json, source text, stop_desc text, type GtfsStop_type, wheelChairAccessible wheelChairAccessible_type, zoneCode text);
+/* (Beta) Export of data model GtfsStop of the subject dataModel.UrbanMobility for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE GtfsStop_type AS ENUM ('GtfsStop');CREATE TYPE wheelChairAccessible_type AS ENUM ('0','1','2');
+CREATE TABLE GtfsStop (address JSON, alternateName TEXT, areaServed TEXT, code TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, hasService TEXT, hasStop JSON, name TEXT, operatedBy JSON, owner JSON, page TEXT, source TEXT, stop_desc TEXT, type GtfsStop_type, wheelChairAccessible wheelChairAccessible_type, zoneCode TEXT);
