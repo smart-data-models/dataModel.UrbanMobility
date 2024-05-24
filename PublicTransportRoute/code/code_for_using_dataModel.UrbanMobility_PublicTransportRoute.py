485806b57257b5a02b88a989950b84dc34e553fe
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:16:39 +0200

    created example of code

--- PublicTransportRoute/code/code_for_using_dataModel.UrbanMobility_PublicTransportRoute.py
+++ PublicTransportRoute/code/code_for_using_dataModel.UrbanMobility_PublicTransportRoute.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "PublicTransportRoute"
 subject = "dataModel.UrbanMobility"
-routeCode = "{'type': 'Property', 'value': '5200103000'}"
+routeCode = "5200103000"
 attribute = "routeCode"
 value = routeCode
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-routeColor = "{'type': 'Property', 'value': '#ff0000'}"
+routeColor = "#ff0000"
 attribute = "routeColor"
 value = routeColor
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-routeSegments = {'type': 'Property', 'value': [{'segmentName': 'PE ACASTILLO-PLAZA DE ITALIA:1', 'refPublicTransportStops': ['urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:311', 'urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:129']}, {'segmentName': 'PE ACASTILLO-PLAZA DE ITALIA:2', 'refPublicTransportStops': ['urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:130', 'urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:131']}]}
+routeSegments = [{'segmentName': 'PE ACASTILLO-PLAZA DE ITALIA:1', 'refPublicTransportStops': ['urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:311', 'urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:129']}, {'segmentName': 'PE ACASTILLO-PLAZA DE ITALIA:2', 'refPublicTransportStops': ['urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:130', 'urn:ngsi-ld:PublicTransportStop:santander:transport:busStop:131']}]
 attribute = "routeSegments"
 value = routeSegments
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-routeTextColor = "{'type': 'Property', 'value': 'RED'}"
+routeTextColor = "RED"
 attribute = "routeTextColor"
 value = routeTextColor
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
