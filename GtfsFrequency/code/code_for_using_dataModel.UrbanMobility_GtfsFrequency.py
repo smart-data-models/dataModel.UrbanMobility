8513cd5585dbd2bedc6989a879daf94e9c919238
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:16:06 +0200

    created example of code

--- GtfsFrequency/code/code_for_using_dataModel.UrbanMobility_GtfsFrequency.py
+++ GtfsFrequency/code/code_for_using_dataModel.UrbanMobility_GtfsFrequency.py
@@ -24,31 +24,36 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "GtfsFrequency"
 subject = "dataModel.UrbanMobility"
-endTime = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2018-03-19T10:25:00Z'}}"
+endTime = "10:25:00"
 attribute = "endTime"
 value = endTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-hasTrip = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:GtfsTrip:Spain:Malaga:1'}"
+hasTrip = "urn:ngsi-ld:GtfsTrip:Spain:Malaga:1"
 attribute = "hasTrip"
 value = hasTrip
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-headwaySeconds = {'type': 'Property', 'value': 600}
+headwaySeconds = 600
 attribute = "headwaySeconds"
 value = headwaySeconds
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-startTime = "{'type': 'Property', 'value': {'@type': 'DateTime', '@value': '2018-03-19T07:00:00Z'}}"
+startTime = "07:00:00"
 attribute = "startTime"
 value = startTime
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
