5e3bb7bfb43a7ab46e19a1ea33f51580f5be78e5
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:16:16 +0200

    created example of code

--- GtfsShape/code/code_for_using_dataModel.UrbanMobility_GtfsShape.py
+++ GtfsShape/code/code_for_using_dataModel.UrbanMobility_GtfsShape.py
@@ -24,8 +24,13 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "GtfsShape"
