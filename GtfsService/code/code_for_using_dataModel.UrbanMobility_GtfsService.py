27b4c6cb145ccb52a392d960a53816e61f2d3b98
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:16:13 +0200

    created example of code

--- GtfsService/code/code_for_using_dataModel.UrbanMobility_GtfsService.py
+++ GtfsService/code/code_for_using_dataModel.UrbanMobility_GtfsService.py
@@ -24,13 +24,18 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "GtfsService"
 subject = "dataModel.UrbanMobility"
-operatedBy = "{'type': 'Relationship', 'object': 'urn:ngsi-ld:GtfsAgency:Malaga_EMT'}"
+operatedBy = "urn:ngsi-ld:GtfsAgency:Malaga_EMT"
 attribute = "operatedBy"
 value = operatedBy
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
