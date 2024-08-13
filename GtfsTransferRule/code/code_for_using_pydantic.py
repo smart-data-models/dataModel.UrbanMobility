c20db95366178a8e133a48de9344e06903ae5ff1
Alberto Abella <alberto.abella@fiware.org>
2024-08-13 18:45:55 +0200

    Draft version of pydantic file v2

---
+++ GtfsTransferRule/code/code_for_using_pydantic.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from enum import Enum
+from typing import List, Optional, Union
+
+from pydantic import AnyUrl, AwareDatetime, BaseModel, Field, confloat, constr
+
+
+class TransferType(Enum):
+    field_0 = 0
+    field_1 = 1
+    field_2 = 2
+    field_3 = 3
+
+
+class Type(Enum):
+    GtfsTransferRule = 'GtfsTransferRule'
+
+
+class GtfsTransferRule(BaseModel):
+    alternateName: Optional[str] = Field(
+        None, description='An alternative name for this item'
+    )
+    dataProvider: Optional[str] = Field(
+        None,
+        description='A sequence of characters identifying the provider of the harmonised data entity',
+    )
+    dateCreated: Optional[AwareDatetime] = Field(
+        None,
+        description='Entity creation timestamp. This will usually be allocated by the storage platform',
+    )
+    dateModified: Optional[AwareDatetime] = Field(
+        None,
+        description='Timestamp of the last modification of the entity. This will usually be allocated by the storage platform',
+    )
+    description: Optional[str] = Field(None, description='A description of this item')
+    hasDestination: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(
+        None,
+        description='Trip associated to this Entity. It shall point to an Entity of type GtfsStop or GtfsStation',
+    )
+    hasOrigin: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(
+        None,
+        description='Trip associated to this Entity. It shall point to an Entity of type GtfsStop or GtfsStation',
+    )
+    id: Optional[
+        Union[
+            constr(
+                pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!, :\\\\]+$',
+                min_length=1,
+                max_length=256,
+            ),
+            AnyUrl,
+        ]
+    ] = Field(None, description='Unique identifier of the entity')
+    minimumTransferTime: Optional[confloat(ge=1.0)] = Field(
+        None, description="Same as GTFS `min_transfer_time`. Unit:'seconds'"
+    )
+    name: Optional[str] = Field(None, description='The name of this item')
+    owner: Optional[
+        List[
+            Union[
+                constr(
+                    pattern=r'^[\\w\\-\\.\\{\\}\\$\\+\\*\\[\\]`|~^@!,:\\\\]+$',
+                    min_length=1,
+                    max_length=256,
+                ),
+                AnyUrl,
+            ]
+        ]
+    ] = Field(
+        None,
+        description='A List containing a JSON encoded sequence of characters referencing the unique Ids of the owner(s)',
+    )
+    seeAlso: Optional[Union[List[AnyUrl], AnyUrl]] = Field(
+        None, description='list of uri pointing to additional resources about the item'
+    )
+    source: Optional[str] = Field(
+        None,
+        description='A sequence of characters giving the original source of the entity data as a URL. Recommended to be the fully qualified domain name of the source provider, or the URL to the source object',
+    )
+    transferType: Optional[TransferType] = Field(
+        None, description="Same as GTFS `transfer_type`. Enum:'0, 1, 2, 3'"
+    )
+    type: Optional[Type] = Field(
+        None, description='NGSI Entity type. It has to be GtfsTransferRule'
+    )
